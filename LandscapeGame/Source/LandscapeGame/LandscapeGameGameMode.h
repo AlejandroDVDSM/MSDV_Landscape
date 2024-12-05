4e5aa6191b563925aab49fdc183679882458213d
AlejandroDVDSM <bysalexx@gmail.com>
2024-12-05 22:17:31 +0000

    Add Unreal Project with a landscape and a third person controller

---
+++ LandscapeGame/Source/LandscapeGame/LandscapeGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LandscapeGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALandscapeGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALandscapeGameGameMode();
+};
+
+
+
